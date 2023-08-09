1875c8f0d1f1fe7a550cf3b9292048f70894d406
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-08-09 17:11:14 +0300

    upd: adds functions

---
+++ contacts.js
@@ -0,0 +1,11 @@
+const fs = require("fs").promises;
+
+//  const contactsPath = ;
+
+function listContacts() {}
+
+function getContactById(contactId) {}
+
+function removeContact(contactId) {}
+
+function addContact(name, email, phone) {}
