c9101a6f74aa6e8255e0f7567583f42b4395fe8f
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-08-11 16:27:09 +0300

    upd: adds functions

--- contacts.js
+++ contacts.js
@@ -1,15 +1,40 @@
+const { nanoid } = require("nanoid");
 const { join } = require("path");
 const fs = require("fs").promises;
 
-const contactsPath = fs.readFile(join(__dirname, "contacts.json"));
+const contactsPath = join(__dirname, "contacts.json");
 
-const listContacts = () => {};
+const listContacts = async () => {
+  return await JSON.parse(fs.readFile(contactsPath));
+};
 
-const getContactById = (contactId) => {};
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result;
+};
 
-const removeContact = (contactId) => {};
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  if (index === -1) {
+    return;
+  }
+  const updatedContacts = contacts.splice(contactIndex, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
+};
 
-const addContact = (name, email, phone) => {};
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  contacts.push({
+    id: nanoid,
+    name,
+    email,
+    phone,
+  });
+};
 
 module.exports = {
   listContacts,
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const contactService = require("./contacts");
 
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contactService.listContacts();
+      return console.log(allContacts);
+      break;
+    case "get":
+      const foundedBook = await contactService.getContactById(id);
+      return console.log(foundedBook);
+      break;
 
-const invokeAction = async ({ action }) => {
-    switch (action) {
-        case 'list'{
-            
-      }
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
 };
